0de530d03febcf6041f7d9b23e2d518ce4281859
Tom Panos <tomp@kindoasis.com>
2025-07-11 14:28:42 -0500

    fix: Add missing src/App.js component
    - Create main App component that renders WisconsinAdvocacyTool
    - Import required CSS file
    - Export default for index.js to import

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WisconsinAdvocacyTool from './WisconsinAdvocacyTool';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WisconsinAdvocacyTool />
+    </div>
+  );
+}
+
+export default App;
